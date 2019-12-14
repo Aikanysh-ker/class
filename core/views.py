decd56834bda68406a55f384246dc8d645017d9b
Aikanysh-ker <kerimkulova.aikanysh@gmail.com>
2019-12-14 12:44:16 +0600

    first commit

---
+++ core/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Student
+
+def homepage (request) :
+    return HttpResponse("hi")
+
+def about (request) :
+    student = Student.objects.first()
+    context = {
+        "name" : student.name,
+        "address" : student.address
+    }   
+    page = "about.html"
+    return render (request, page, context)
